293b94d75cc04a839b9fdda1950b3f0ba11e9c8e
Carl Stevenson <carl4ece@bu.edu>
2024-04-13 13:55:44 -0400

    Centering text

--- lib/main.dart
+++ lib/main.dart
@@ -41,19 +41,22 @@ class MyHomePage extends StatelessWidget {
     var pair = appState.current;
 
     return Scaffold(
-      body: Column(
-        children: [
-          Text('Hello World! Suggested name for new world:'),
-          WorldCard(pair: pair),
-          
-          ElevatedButton(
-            onPressed: () {
-              appState.getNext();
-            },
-            child: Text('Next'),
-          ),
-
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Hello World! Suggested name for new world:'),
+            WorldCard(pair: pair),
+            
+            ElevatedButton(
+              onPressed: () {
+                appState.getNext();
+              },
+              child: Text('Next'),
+            ),
+        
+          ],
+        ),
       ),
     );
   }
